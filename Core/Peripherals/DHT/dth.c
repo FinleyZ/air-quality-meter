dfa9619683aefe68b5827c593d6f38ec413de5e5
Finley <hongfeiz@mun.ca>
2025-04-08 18:25:30 -0600

    first commit

---
+++ Core/Peripherals/DHT/dth.c
@@ -0,0 +1,18 @@
+#include "dht.h"
+
+void DHT_SetPinOutput(void) {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Pin = GPIO_PIN_8;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+void DHT_SetPinInput(void) {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Pin = GPIO_PIN_8;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
---
+++ Core/Peripherals/DHT/dth.h
@@ -0,0 +1,8 @@
+#ifndef __DHT11_H
+#define __DHT11_H
+
+
+void DHT_SetPinOutput(void);
+void DHT_SetPinInput(void);
+
+#endif
